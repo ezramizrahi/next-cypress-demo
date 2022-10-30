905901262efccd8450381397bebe66da2eaca5e5
ezramizrahi <mizrahi.ez@gmail.com>
2022-10-31 09:54:13 +1100

    remove unused import

--- pages/films.js
+++ pages/films.js
@@ -1,11 +1,21 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useSession } from "next-auth/react";
 
-export default () => {
+export default function Films() {
+    const { status } = useSession({
+        required: true,
+        // TODO:
+        // determine what this does
+        onUnauthenticated() {
+          return <div>Not authenticated</div>;
+        },
+    });
+    if (status === "loading") {
+        return "Loading or not authenticated..."
+    };
     return (
         <div>
+            <div>user is logged in</div>
             <a href='https://www.youtube.com/watch?v=5qap5aO4i9A'>LoFi Music</a>
         </div>
     );
 };
-
-export const getServerSideProps = withPageAuthRequired();

c569210e854b610e3d9dd3f96848b1f3156abc87
ezramizrahi <mizrahi.ez@gmail.com>
2022-11-14 09:14:59 +1100

    remove target from config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Target must be serverless
-  // for netlify server-side rendering
-  target: "serverless",
 }
 
 module.exports = nextConfig

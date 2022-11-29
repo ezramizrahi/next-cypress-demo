5f14f4a5dd058ac3fdb3be7fcb9f73351b3120ad
ezramizrahi <mizrahi.ez@gmail.com>
2022-11-29 19:47:20 +1100

    add log

--- cypress/support/PageObjects/Common.js
+++ cypress/support/PageObjects/Common.js
@@ -2,7 +2,7 @@ class Common {
     // visit baseUrl defined in cypress.config.js
     visitBaseUrl() {
         cy.visit(Cypress.env('baseUrl'));
-        cy.log('WHAT IS THE URL', Cypress.env('baseUrl'));
+        cy.log('WHAT IS THE URL? ', Cypress.env('baseUrl'));
     };
 };
 

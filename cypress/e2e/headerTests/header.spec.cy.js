e82eda3e7c48ba8523ae0ab867615d76fd0514d1
ezramizrahi <mizrahi.ez@gmail.com>
2022-11-30 06:18:39 +1100

    update cypress test

--- cypress/e2e/headerTests/header.spec.cy.js
+++ cypress/e2e/headerTests/header.spec.cy.js
@@ -5,13 +5,13 @@ describe('SOME TEST', () => {
     beforeEach(() => {
         // visit baseUrl before each test
         Common.visitBaseUrl();
-        const currurl = cy.url();
-        expect(currurl).to.not.equal('https://next-cypress-demo.vercel.app/');
     });
 
     it('should have a log in button', () => {
         // Check that the search button exists in the DOM and is visible
         Header.getLogInButton().should('exist');
         Header.getLogInButton().should('be.visible');
+        const currurl = cy.url();
+        expect(currurl).to.not.equal('https://next-cypress-demo.vercel.app/');
     });
 });
